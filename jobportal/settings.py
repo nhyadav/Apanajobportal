5f4ddf2fdfdee5a929392278b2157fa9e4e38e5d
narayan <42175861+nhyadav@users.noreply.github.com>
2022-01-22 16:56:01 +0530

    database issues solved

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -89,12 +89,24 @@ WSGI_APPLICATION = 'jobportal.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(os.path.join(BASE_DIR, "db.sqlite3"))
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ddi4gbgjk5sirk',
+        'USER': 'ekzxunvreppxuz',
+        'PASSWORD': '5c33211ed3e20cdac720484bcb146371ec8646f469654876bc31a3d2ac29e2e2',
+        'HOST': 'ec2-52-70-120-204.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
