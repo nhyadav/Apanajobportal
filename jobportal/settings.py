bd006e5d19392a7aa27665ef24ea10b2647d2a68
narayan <42175861+nhyadav@users.noreply.github.com>
2022-01-22 17:27:54 +0530

    solve database problem

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -96,6 +96,10 @@ DATABASES = {
     }
 }
 
+
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 # DATABASES = {
 #     'default':{
 #         'ENGINE': 'django.db.backends.postgresql',
